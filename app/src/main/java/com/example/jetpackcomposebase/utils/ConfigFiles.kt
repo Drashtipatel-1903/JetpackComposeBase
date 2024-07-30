31ebbd0210374cbff6e67e27de727c2f8ff6a22f
drashti_patel <drashti.patel@brainvire.com>
2024-07-30 12:37:08 +0530

    - First init commit - Jetpack compose base
    - add sentry
    - add Home screen api
    - add bottom navigtion and navigation drawer

--- app/src/main/java/com/example/jetpackcomposebase/utils/ConfigFiles.kt
+++ app/src/main/java/com/example/jetpackcomposebase/utils/ConfigFiles.kt
@@ -1,9 +1,5 @@
 package com.example.jetpackcomposebase.utils
 
 object ConfigFiles {
-//    const val DEV_BASE_URL = "https://wellness.brainvire.dev/wellness-apis/public/front/api/"
-//    const val DEV_BASE_URL = "https://62cbcd8ea0800529309ee97b.mockapi.io/api/v1/"
-
-    //const val DEV_BASE_URL = "https://androidfirst.free.beeceptor.com/my/api/"
      const val DEV_BASE_URL = "https://hp-api.herokuapp.com/api/"
 }
