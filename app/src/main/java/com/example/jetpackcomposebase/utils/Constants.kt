224cd0e8ad6a3ecc82af73bde06661b7d99366d5
drashti_patel <drashti.patel@brainvire.com>
2024-07-30 12:42:07 +0530

    - First init commit - Jetpack compose base
    - add sentry
    - add Home screen api
    - add bottom navigtion and navigation drawer

--- app/src/main/java/com/example/jetpackcomposebase/utils/Constants.kt
+++ app/src/main/java/com/example/jetpackcomposebase/utils/Constants.kt
@@ -9,11 +9,8 @@ object Constants {
 
     var KEY_GRAPH_ID = "graphId"
     var KEY_START_DESTINATION = "startDestination"
-    var TERMS_CONDITION_URL = "    https://aichatapp.app/terms-of-use"
-    var PRIVACY_POLICY_URL = "https://aichatapp.app/privacy-and-cookies-policy"
 
     var DSN = "https://e68671b99bf2afd717617c18a3aa1dc4@o4507655094140928.ingest.us.sentry.io/4507666947309568"
-    const val ALL_QUOTES = "quotes"
 
 
     object DataStore {
